da8c35db044c5ff5993f2fba242660fb2a78e75b
Nightwolf <nico.style931@gmail.com>
2018-05-04 00:51:12 +0200

    protocol

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"github.com/nightwolf93/transmitter-server/config"
 	"github.com/nightwolf93/transmitter-server/logging"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	config.SetupConfig()
 	logging.SetupLog()
-
-	log.Infof("Config value : %s", config.GetConfig().Log.Path)
 }
